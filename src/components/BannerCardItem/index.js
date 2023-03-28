d973ef59b2b59538a4b68d75bfe30feae625a607
Shitalabathorat <126850348+Shitalabathorat@users.noreply.github.com>
2023-03-28 12:13:49 +0530

    Update index.js

--- src/components/BannerCardItem/index.js
+++ src/components/BannerCardItem/index.js
@@ -1,17 +1,19 @@
-// Write your code here.
 import './index.css'
-const BannerProfile = props => {
-  const {bannerDetails} = props
-  const {id, headerText, description, className} = bannerDetails
+
+const UserProfile = props => {
+  const {userDetails} = props
+  const {headerText, description, className} = userDetails
 
   return (
-    <div className="banner-card-container">
+    <li className="user-card-container">
       <div className={className}>
-        <h1 class="heading">{headerText}</h1>
+        <h1 className="head">{headerText}</h1>
         <p className="para">{description}</p>
-        <button className="btn">Show More</button>
+        <button className="btn" type="button">
+          Show More
+        </button>
       </div>
-    </div>
+    </li>
   )
 }
-export default BannerProfile
+export default UserProfile
